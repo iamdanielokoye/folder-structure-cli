f85268c0c6f12e97d966e16b3063cc7771007bfa
Daniel Okoye <okoyedann@gmail.com>
2025-03-03 14:50:38 +0100

    Update setup.py to set version directly and clean up update script

--- scripts/update.sh
+++ scripts/update.sh
@@ -4,16 +4,10 @@ echo "Updating Folder Structure CLI Tool to the latest version..."
 
 git pull origin main
 
-pipx ensurepath
 pipx install --force -e ./scripts
 
 echo "✅ CLI Tool updated successfully"
 
-# Fetch and display the latest release tag from GitHub
-latest_tag=$(curl -s "https://api.github.com/repos/iamdanielokoye/folder-structure-cli/releases/latest" | jq -r '.tag_name')
+pipx ensurepath
 
-if [ -n "$latest_tag" ]; then
-  echo "Latest release tag from GitHub: $latest_tag"
-else
-  echo "Failed to fetch latest release tag from GitHub."
-fi
+echo "✅ Path updated successfully, restart your terminal to see changes!"
