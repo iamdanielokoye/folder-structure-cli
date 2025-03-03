29c2d714ac4f833a0bad02ada5e1edb2d1a3e2f7
Daniel Okoye <okoyedann@gmail.com>
2025-03-03 13:49:48 +0100

    Change requests installation from pipx to pip in update script

--- scripts/update.sh
+++ scripts/update.sh
@@ -4,7 +4,7 @@ echo "Updating Folder Structure CLI Tool to the latest version..."
 
 git pull origin main
 
-pipx install requests
+pip install requests
 pipx install --force -e ./scripts
 
 echo "✅ CLI Tool updated successfully"
