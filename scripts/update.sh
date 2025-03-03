eaeb606cc59752b6339eee6458169eba18543ab7
Daniel Okoye <okoyedann@gmail.com>
2025-03-03 13:44:18 +0100

    Update update script to install CLI tool from the scripts directory

--- scripts/update.sh
+++ scripts/update.sh
@@ -4,6 +4,6 @@ echo "Updating Folder Structure CLI Tool to the latest version..."
 
 git pull origin main
 
-pipx install --force -e .
+pipx install --force -e ./scripts
 
 echo "✅ CLI Tool updated successfully"
