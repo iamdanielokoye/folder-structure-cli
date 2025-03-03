07268bb10178735d624bba157f153d0d2790f69f
Daniel Okoye <okoyedann@gmail.com>
2025-03-03 11:33:06 +0100

    Add new JSON structure example and update setup script to fetch latest release tag

--- scripts/setup.py
+++ scripts/setup.py
@@ -1,8 +1,16 @@
+import requests
 from setuptools import setup, find_packages
+
+def get_latest_release_tag():
+    url = "https://api.github.com/repos/iamdanielokoye/folder-structure-cli/releases/latest"
+    response = requests.get(url)
+    response.raise_for_status()
+    latest_release = response.json()
+    return latest_release["tag_name"]
 
 setup(
     name="folder-structure-cli",
-    version="0.1.0",
+    version=get_latest_release_tag(),
     packages=find_packages(where="../src"),  # Look for packages inside 'src'
     package_dir={'': '..'},  # Treat 'src' as the root package directory
     
